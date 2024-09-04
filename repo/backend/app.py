cb8b286f1e44ca454dffa7e66b9e3ea60ced816b
atharva <bhandvalkaratharva@gmail.com>
2024-09-04 17:43:02 +0530

    changed backend flask routes

--- repo/backend/app.py
+++ repo/backend/app.py
@@ -3,19 +3,19 @@ from random import randint
 from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+@app.route('/app/')
 def index():
     return render_template('index.html')
 
-@app.route('/test')
+@app.route('/app/test')
 def test():
     return render_template('test.html')
 
-@app.route('/info')
+@app.route('/app/info')
 def info():
     return render_template('info.html')
 
-@app.route('/random')
+@app.route('/app/random')
 def random():
     return f'{randint(0,10)}'
 
